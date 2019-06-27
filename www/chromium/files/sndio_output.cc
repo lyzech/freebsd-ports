6d790e6cad7734715ca2a06d955d8bea879b8ec1
Tobias Kortkamp <t@tobik.me>
2019-06-27 06:29:22 +0200

    chromium: sndio: allow for independent setting of input and output audiodevices
    Signed-off-by: Tobias Kortkamp <t@tobik.me>

--- www/chromium/files/sndio_output.cc
+++ www/chromium/files/sndio_output.cc
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <cstdlib>
+
 #include "base/logging.h"
 #include "base/time/time.h"
 #include "base/time/default_tick_clock.h"
@@ -64,7 +66,7 @@ bool SndioAudioOutputStream::Open() {
   par.le = SIO_LE_NATIVE;
   par.appbufsz = params.frames_per_buffer();
 
-  hdl = sio_open(SIO_DEVANY, SIO_PLAY, 0);
+  hdl = sio_open(getenv("PLAY_AUDIODEVICE"), SIO_PLAY, 0);
   if (hdl == NULL) {
     LOG(ERROR) << "Couldn't open audio device.";
     return false;
