cabf22148dce9597449ef330f2fe9e33c865b97c
Charlie Root <root@none>
2019-12-26 17:52:11 +0100

    cad/prusa-slicer: correct patch

---
+++ cad/prusa-slicer/files/patch-src_avrdude_libavrdude.h
@@ -0,0 +1,10 @@
+--- src/avrdude/libavrdude.h.orig	2019-09-16 08:49:51 UTC
++++ src/avrdude/libavrdude.h
+@@ -950,7 +950,7 @@ int read_config_builtin();
+ // Header file for alloca()
+ #if defined(WIN32NATIVE)
+ #  include <malloc.h>
+-#else
++#elif !defined(__FreeBSD__)
+ #  include <alloca.h>
+ #endif
