3ccd9adf0438f97edc2a6d759b1ade831140d8d7
Tobias Kortkamp <t@tobik.me>
2020-07-12 09:18:26 +0200

    sndio: regen custom patch
    Signed-off-by: Tobias Kortkamp <t@tobik.me>

--- audio/sndio/files/patch-libsndio_aucat.c
+++ audio/sndio/files/patch-libsndio_aucat.c
@@ -1,7 +1,7 @@
---- libsndio/aucat.c.orig	2018-01-11 20:57:37 UTC
+--- libsndio/aucat.c.orig	2020-06-28 05:52:52 UTC
 +++ libsndio/aucat.c
-@@ -247,7 +247,7 @@ aucat_mkcookie(unsigned char *cookie)
- #define COOKIE_SUFFIX	"/.aucat_cookie"
+@@ -248,7 +248,7 @@ aucat_mkcookie(unsigned char *cookie)
+ #define COOKIE_SUFFIX	"/.sndio/cookie"
  #define TEMPL_SUFFIX	".XXXXXXXX"
  	struct stat sb;
 -	char *home, *path = NULL, *tmp = NULL;
@@ -9,7 +9,7 @@
  	size_t home_len, path_len;
  	int fd, len;
  
-@@ -257,6 +257,11 @@ aucat_mkcookie(unsigned char *cookie)
+@@ -258,6 +258,11 @@ aucat_mkcookie(unsigned char *cookie)
  	/*
  	 * try to load the cookie
  	 */
@@ -21,11 +21,11 @@
  	home = issetugid() ? NULL : getenv("HOME");
  	if (home == NULL)
  		goto bad_gen;
-@@ -267,6 +272,7 @@ aucat_mkcookie(unsigned char *cookie)
+@@ -268,6 +273,7 @@ aucat_mkcookie(unsigned char *cookie)
  	memcpy(path, home, home_len);
  	memcpy(path + home_len, COOKIE_SUFFIX, sizeof(COOKIE_SUFFIX));
  	path_len = home_len + sizeof(COOKIE_SUFFIX) - 1;
 +open_cookie:
  	fd = open(path, O_RDONLY);
- 	if (fd < 0) {
+ 	if (fd == -1) {
  		if (errno != ENOENT)
